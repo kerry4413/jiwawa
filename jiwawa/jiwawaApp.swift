a9f335ac774f58e0dad15eb9852e31a24021e214
dejes <jack940197@gmail.com>
2020-11-03 19:57:04 +0800

    Initial Commit

---
+++ jiwawa/jiwawaApp.swift
@@ -0,0 +1,17 @@
+//
+//  jiwawaApp.swift
+//  jiwawa
+//
+//  Created by User14 on 2020/11/3.
+//
+
+import SwiftUI
+
+@main
+struct jiwawaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
